4d94b068d8c8b22c296f9c2a55746f25a3e8275b
dhillenbp179 <dhillen.mutu@gmail.com>
2023-07-25 16:12:34 +0700

    add express as a backend

---
+++ backend/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./src/routes/auth");
+const blogRoutes = require("./src/routes/blog");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const port = 4000;
+const host = "localhost";
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+
+// alternatif
+// const cors = require("cors");
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Header", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI) // replace with your driver mongo atlas
+  .then(() => {
+    app.listen(port, host, () => {
+      console.log(`Blog News | listening at http://${host}:${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ backend/src/controllers/blog.js
@@ -0,0 +1,187 @@
+const { validationResult } = require("express-validator");
+const BlogPost = require("../models/blog");
+const path = require("path");
+const fs = require("fs");
+
+exports.createBlogPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  // jika ada error
+  if (!errors.isEmpty()) {
+    const err = new Error("Input Value Tidak Sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
+  // mengecek file tidak ada
+  if (!req.file) {
+    const err = new Error("Image harus diupload");
+    err.errorStatus = 422;
+    err.data = errors.array();
+    throw err;
+  }
+
+  const title = req.body.title;
+  const image = req.file.path;
+  const body = req.body.body;
+
+  const Posting = new BlogPost({
+    title: title,
+    body: body,
+    image: image,
+    author: {
+      uid: 1,
+      name: "Author",
+    },
+  });
+
+  Posting.save()
+    .then((result) => {
+      res
+        .status(201)
+        .json({ message: "Create Blog Post Success", data: result });
+    })
+    .catch((err) => {
+      console.log("err ", err);
+    });
+};
+
+exports.getAllBlogPost = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = req.query.perPage || 5;
+  let totalItems;
+
+  BlogPost.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return BlogPost.find()
+        .skip((parseInt(currentPage) - 1) * parseInt(perPage))
+        .limit(parseInt(perPage));
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Data Blog Post berhasil dipanggil",
+        data: result,
+        total_data: totalItems,
+        per_page: perPage,
+        current_page: currentPage,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+
+  // BlogPost.find()
+  //   .then((result) => {
+  //     res.status(200).json({
+  //       message: "Data Blog Post berhasil dipanggil",
+  //       data: result,
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     next(err);
+  //   });
+};
+
+exports.getBlogPostById = (req, res, next) => {
+  const postId = req.params.postId;
+  BlogPost.findById(postId)
+    .then((result) => {
+      if (!result) {
+        const error = new Error("Input Value Tidak Sesuai");
+        error.errorStatus = 404;
+        throw error;
+      }
+      res.status(200).json({
+        message: "Data Blog Post berhasil dipanggil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.updateBlogPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  // jika ada error
+  if (!errors.isEmpty()) {
+    const err = new Error("Input Value Tidak Sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
+  // mengecek file tidak ada
+  if (!req.file) {
+    const err = new Error("Image harus diupload");
+    err.errorStatus = 422;
+    err.data = errors.array();
+    throw err;
+  }
+
+  const title = req.body.title;
+  const image = req.file.path;
+  const body = req.body.body;
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Blog Post tidak ditemukan");
+        err.status = 404;
+        throw err;
+      }
+      post.title = title;
+      post.body = body;
+      post.image = image;
+
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Update Success",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.deleteBlogPost = (req, res, next) => {
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("Blog Post tidak ditemukan");
+        err.status = 404;
+        throw err;
+      }
+      removeImage(post.image);
+      return BlogPost.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Hapus Blog Post Berhasil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+const removeImage = (filePath) => {
+  // console.log("filePath ", filePath);
+  // console.log("dirname ", __dirname);
+
+  filePath = path.join(__dirname, "../..", filePath);
+  fs.unlink(filePath, (err) => {
+    console.log(err);
+  });
+};
